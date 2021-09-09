344f11d296a7f8467884bd4701da62b6582cb17c
kumaran Iyyasamy srinivasan <kumaran.x.iyyasamy.srinivasan.-nd@disney.com>
2021-09-08 23:22:19 -0400

    initial check-in

---
+++ CHANGELOG.md
@@ -0,0 +1,11 @@
+# Change log
+
+## 0.0.1 (2021-09-08)
+
+### Features
+
+* Initial Spring Boot Project Setup
+
+### Bug Fix
+
+* NA
---
+++ src/main/java/com/college/demo/SpringbootSpringdatajdbcMysqlDemoApplication.java
@@ -0,0 +1,13 @@
+package com.college.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpringdatajdbcMysqlDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootSpringdatajdbcMysqlDemoApplication.class, args);
+	}
+
+}
